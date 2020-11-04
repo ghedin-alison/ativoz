a14b22ef3bf210b339da4d000a5afcf2f710da6b
Alison Ghedin <alison.ghedin@outlook.com>
2020-11-04 16:36:10 -0300

    atualizando static files

--- ativoz/urls.py
+++ ativoz/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('gerex/', admin.site.urls),
     path('', include('core.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
